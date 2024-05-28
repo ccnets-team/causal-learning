acceb79a2fc8b61911f6376428df1daa9b20e4fd
junho-deepdream <75417171+junho-deepdream@users.noreply.github.com>
2024-05-28 14:28:26 +0900

    feat: Complete integration of PyTorch TabNet as a component to form a cooperative network

--- nn/tabnet.py
+++ nn/tabnet.py
@@ -1,6 +1,6 @@
 # Check and import pytorch_tabnet if installed, else show an error message
 try:
-    from pytorch_tabnet.tab_network import TabNetNoEmbeddings
+    from pytorch_tabnet.tab_network import TabNetEncoder as TabNetwork
 except ImportError:
     print("Error: pytorch-tabnet library is not installed. Please install it using 'pip install pytorch-tabnet'.")
     raise
@@ -14,15 +14,20 @@ class TabNet(nn.Module):
         d_model = network_params.d_model
         num_layers = network_params.num_layers
         
+        # Initialize group attention matrix with ones and move to appropriate device
         group_attention_matrix = torch.ones(1, d_model).to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
 
-        self.network = TabNetNoEmbeddings(
+        self.network = TabNetwork(
             input_dim=d_model,
             output_dim=d_model,
+            n_d = d_model,
+            n_a = d_model,
             n_steps = num_layers,
             group_attention_matrix = group_attention_matrix
         )
 
     def forward(self, x, padding_mask=None):
-        results, _ = self.network(x)
-        return results
+        steps_output, _ = self.network(x)
+        # steps_output is a list of tensors
+        res = torch.sum(torch.stack(steps_output, dim=0), dim=0)
+        return res
