f6fc694d628b88f8eb035b344fa1b4f7b456dd62
junho-deepdream <75417171+junho-deepdream@users.noreply.github.com>
2024-06-11 12:02:56 +0900

    feat: Refactored and cleaned up TestReport class

--- tools/report.py
+++ tools/report.py
@@ -19,7 +19,32 @@ def transform_labels_to_original_scale(inferred_y, target_y, task_type, label_si
             target_y = target_y * tensor_scale
     return inferred_y, target_y
 
-def calculate_test_results(inferred_y, target_y, task_type, label_size, label_scale, average='macro'):
+def calculate_test_results(inferred_y, target_y, task_type, label_size, label_scale):
+    inferred_y, target_y = transform_labels_to_original_scale(inferred_y, target_y, task_type, label_size, label_scale)
+    num_classes = label_size
+    
+    if task_type == 'binary_classification' or task_type == 'multi_class_classification':
+        inferred_y = torch.argmax(inferred_y, dim=-1)
+        target_y = torch.argmax(target_y, dim=-1)  # Assuming one-hot encoding of target
+    elif task_type == "ordinal_regression":
+        inferred_y = torch.round(inferred_y).int().clamp(0, num_classes - 1)
+        target_y = torch.round(target_y).int()
+    
+    return get_test_results(inferred_y, target_y, task_type, num_classes)
+
+def convert_to_tensor(values):
+    if isinstance(values, np.ndarray):
+        if values.dtype == np.float32 or values.dtype == np.float64:
+            values = torch.tensor(values, dtype=torch.float32)
+        elif values.dtype == np.int32 or values.dtype == np.int64:
+            values = torch.tensor(values, dtype=torch.int64)
+        else:
+            values = torch.tensor(values)
+    else:
+        values = torch.tensor(values)
+    return values
+
+def get_test_results(inferred_y, target_y, task_type, num_classes, average='macro'):
     """
     Calculates performance metrics for tasks using PyTorch tensors that might have batch and sequence dimensions.
     Parameters:
@@ -34,20 +59,17 @@ def calculate_test_results(inferred_y, target_y, task_type, label_size, label_sc
     """
     metrics = {}
     
-    num_classes = label_size
-    inferred_y, target_y = transform_labels_to_original_scale(inferred_y, target_y, task_type, label_size, label_scale)
-    
+    # Check inferred_y is tensor
+    if not isinstance(inferred_y, torch.Tensor):
+        inferred_y = convert_to_tensor(inferred_y)
+    if not isinstance(target_y, torch.Tensor):
+        target_y = convert_to_tensor(target_y)
+
     # Move tensors to CPU for compatibility with sklearn metrics
     inferred_y = inferred_y.cpu()
     target_y = target_y.cpu()
 
     if task_type in ['binary_classification', 'multi_class_classification', 'ordinal_regression']:
-        if task_type == 'binary_classification' or task_type == 'multi_class_classification':
-            inferred_y = torch.argmax(inferred_y, dim=-1)
-            target_y = torch.argmax(target_y, dim=-1)  # Assuming one-hot encoding of target
-        elif task_type == "ordinal_regression":
-            inferred_y = torch.round(inferred_y).int().clamp(0, num_classes - 1)
-            target_y = torch.round(target_y).int()
 
         inferred_y = inferred_y.flatten()
         target_y = target_y.flatten()
@@ -82,3 +104,4 @@ def calculate_test_results(inferred_y, target_y, task_type, label_size, label_sc
         metrics['r2'] = r2.item()
 
     return metrics
+    
