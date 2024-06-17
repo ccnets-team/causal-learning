fix: Update causal-learning API to handle binary classification labels without one-hot encoding
- Adjusted causal-learning API to process binary classification labels as single-dimension arrays.
- Eliminates internal one-hot encoding for binary classification, simplifying the label processing to use dimension size of 1.

framework/utils/ccnet_utils.py
@@ -49,7 +49,7 @@ def determine_activation_by_task_type(task_type):
     - ValueError: If an unsupported task type is provided.
     """
     if task_type in ["binary_classification"]: # binary classification is one-hot encoded
-        return 'none'
+        return 'sigmoid'
     elif task_type in ["multi_class_classification", "compositional_regression"]: # binary classification is one-hot encoded
         return 'softmax'
     elif task_type in ["multi_label_classification"]:
@@ -73,7 +73,10 @@ def generate_condition_data(label_shape, task_type, device):
     Returns:
     - Tensor: A tensor of condition data appropriate for the specified task type, all in float dtype.
     """
-    if task_type in ["binary_classification", "multi_class_classification", "compositional_regression"]: # binary classification is one-hot encoded
+    if task_type in ["binary_classification"]:
+        condition_data = torch.rand(label_shape).to(device)
+        condition_data = (condition_data > 0.5).float()
+    elif task_type in ["multi_class_classification", "compositional_regression"]: 
         logits = torch.randn(label_shape).to(device)
         # Use softmax to simulate probabilities across classes
         condition_data = torch.softmax(logits, dim=-1)

tools/report.py
@@ -21,8 +21,11 @@ def transform_labels_to_original_scale(inferred_y, target_y, task_type, label_si
 
 def calculate_test_results(inferred_y, target_y, task_type, label_size, label_scale):
     inferred_y, target_y = transform_labels_to_original_scale(inferred_y, target_y, task_type, label_size, label_scale)
-    
-    if task_type == 'binary_classification' or task_type == 'multi_class_classification':
+        
+    if task_type == 'binary_classification':
+        inferred_y = (inferred_y > 0.5).int()
+        target_y = (target_y > 0.5).int()
+    elif task_type == 'multi_class_classification':
         inferred_y = torch.argmax(inferred_y, dim=-1)
         target_y = torch.argmax(target_y, dim=-1)  # Assuming one-hot encoding of target
     elif task_type == "ordinal_regression":
@@ -102,7 +105,7 @@ def get_test_results(inferred_y, target_y, task_type, num_classes, average='macr
         print("inferred_y is not tensor")
     if not isinstance(target_y, torch.Tensor):
         target_y = convert_to_tensor(target_y)
-
+        
     # Move tensors to CPU for compatibility with sklearn metrics
     inferred_y = inferred_y.cpu()
     target_y = target_y.cpu()

tools/tensor_utils.py
@@ -161,17 +161,10 @@ def convert_to_one_hot(target_batch, label_size, task_type):
     if target_batch is None or label_size is None or task_type is None:
         return None
     
-    # Mask to identify locations with -1 which should remain unchanged after encoding
-    target_label_size = None
-    if task_type == 'binary_classification':
-        target_label_size = 2
-    elif task_type == 'multi_class_classification':
-        target_label_size = label_size
-
-    if target_label_size is not None and target_batch.shape[-1] != target_label_size:
+    if task_type == 'multi_class_classification' and target_batch.shape[-1] != label_size:
         mask = target_batch == -1
         target_batch[mask] = 0
-        target_batch = torch.nn.functional.one_hot(target_batch.long(), num_classes=target_label_size).float().squeeze(-2)
+        target_batch = torch.nn.functional.one_hot(target_batch.long(), num_classes=label_size).float().squeeze(-2)
         expanded_mask = mask.expand_as(target_batch)    
         target_batch[expanded_mask] = -1
     return target_batch